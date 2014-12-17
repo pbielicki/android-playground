8d7e693b5d4390dfd79912fca18542b197cbe807
pbielicki <przemyslaw.bielicki@amadeus.com>
2014-12-17 08:47:12 +0100

    added Crime domain class

---
+++ CriminalIntent/src/com/bielu/criminalintent/Crime.java
@@ -0,0 +1,54 @@
+package com.bielu.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+  private UUID mId;
+  private String mTitle;
+  private Date mDate;
+  private boolean mSolved;
+  
+  public Crime() {
+  }
+  
+  public Crime(UUID id, String title) {
+    mId = id;
+    mTitle = title;
+    mDate = new Date();
+    mSolved = false;
+  }
+
+  public UUID getId() {
+    return mId;
+  }
+
+  public void setId(UUID id) {
+    mId = id;
+  }
+
+  public String getTitle() {
+    return mTitle;
+  }
+
+  public void setTitle(String title) {
+    mTitle = title;
+  }
+
+  public Date getDate() {
+    return mDate;
+  }
+
+  public void setDate(Date date) {
+    mDate = date;
+  }
+
+  public boolean isSolved() {
+    return mSolved;
+  }
+
+  public void setSolved(boolean solved) {
+    mSolved = solved;
+  }
+}
