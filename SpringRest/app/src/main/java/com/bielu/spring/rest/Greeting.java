ec517119ae4a504a75a94b376b8234b04d4a10d4
pbielicki <przemyslaw.bielicki@amadeus.com>
2014-12-23 09:11:33 +0100

    initial import - Spring Rest template example - using Android Studio

---
+++ SpringRest/app/src/main/java/com/bielu/spring/rest/Greeting.java
@@ -0,0 +1,16 @@
+package com.bielu.spring.rest;
+
+public class Greeting {
+
+  private String id;
+  private String content;
+
+  public String getId() {
+    return this.id;
+  }
+
+  public String getContent() {
+    return this.content;
+  }
+
+}
