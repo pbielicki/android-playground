1d1cb9245ca49724849ab6e8148f0cb34d23e83a
pbielicki <przemyslaw.bielicki@amadeus.com>
2014-12-26 15:48:38 +0100

    Added dialog to change crime date

--- CriminalIntent/src/com/bielu/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/com/bielu/criminalintent/CrimeFragment.java
@@ -1,9 +1,13 @@
 package com.bielu.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -17,6 +21,8 @@ import android.widget.EditText;
 public class CrimeFragment extends Fragment {
 
   public static final String EXTRA_CRIME_ID = "CrimeFragment.CrimeId";
+  protected static final String DIALOG_DATE = "Date";
+  protected static final int REQUEST_DATE = 0;
   private Crime mCrime;
   private EditText mTitleField;
   private Button mDateButton;
@@ -52,7 +58,15 @@ public class CrimeFragment extends Fragment {
     
     mDateButton = (Button) v.findViewById(R.id.crime_date);
     mDateButton.setText(mCrime.getDate().toString());
-    mDateButton.setEnabled(false);
+    mDateButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+        dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+        dialog.show(fm, DIALOG_DATE);
+      }
+    });
     
     mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
     mSolvedCheckBox.setChecked(mCrime.isSolved());
@@ -64,6 +78,19 @@ public class CrimeFragment extends Fragment {
     });
     
     return v;
+  }
+  
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (resultCode != Activity.RESULT_OK) {
+      return;
+    }
+    
+    if (requestCode == REQUEST_DATE) {
+      Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+      mCrime.setDate(date);
+      mDateButton.setText(date.toString());
+    }
   }
   
   public static CrimeFragment newInstance(UUID crimeId) {
---
+++ CriminalIntent/src/com/bielu/criminalintent/DatePickerFragment.java
@@ -0,0 +1,74 @@
+package com.bielu.criminalintent;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.DatePicker;
+
+public class DatePickerFragment extends DialogFragment {
+  
+  public static final String EXTRA_DATE = "DatePickerFragment.Date";
+  private Date mDate;
+  
+  public static DatePickerFragment newInstance(Date date) {
+    Bundle args = new Bundle();
+    args.putSerializable(EXTRA_DATE, date);
+    
+    DatePickerFragment fragment = new DatePickerFragment();
+    fragment.setArguments(args);
+    
+    return fragment;
+  }
+
+
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    mDate = (Date) getArguments().getSerializable(EXTRA_DATE);
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(mDate);
+    
+    int year = cal.get(Calendar.YEAR);
+    int month = cal.get(Calendar.MONTH);
+    int day = cal.get(Calendar.DAY_OF_MONTH);
+    View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+    
+    DatePicker datePicker = (DatePicker) v.findViewById(R.id.dialog_date_datePicker);
+    datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+      @Override
+      public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        mDate = new GregorianCalendar(year, monthOfYear, dayOfMonth).getTime();
+        getArguments().putSerializable(EXTRA_DATE, mDate);
+      }
+    });
+    
+    return new AlertDialog.Builder(getActivity())
+        .setView(v)
+        .setTitle(R.string.date_picker_title)
+        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            sendResult(Activity.RESULT_OK);
+          }
+        })
+        .create();
+  }
+  
+  private void sendResult(int result) {
+    if (getTargetFragment() == null) {
+      return;
+    }
+    
+    Intent i = new Intent();
+    i.putExtra(EXTRA_DATE, mDate);
+    getTargetFragment().onActivityResult(getTargetRequestCode(), result, i);
+  }
+}
